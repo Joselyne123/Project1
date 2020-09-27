76e2033af7243c8d63e42eb3ae3f62e26feb336c
joseingabire123@gmail.com <joseingabire123@gmail.com>
2020-09-27 18:24:18 +0200

    add formula

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ function formCal(){
     // console.log(mm);
     var gender=document.getElementById("gender").value;
     console.log(gender);
-   
+    var c=(yyyy-1)/100 +1;
+    var cc= parseFloat(c);
+    var day=parseFloat( ( (cc/4) -2*cc-1) + ((5*yyyy/4) ) + ((26*(mm+1)/10)) + dd )%7;
+    var day=(day.toFixed(0));
+    // console.log(day);
