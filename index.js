dfbdb87a577eb635c6609f50ec0b885ab868bcdf
joseingabire123@gmail.com <joseingabire123@gmail.com>
2020-09-27 18:26:03 +0200

    add condition for female

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function formCal(){
     var day=parseFloat( ( (cc/4) -2*cc-1) + ((5*yyyy/4) ) + ((26*(mm+1)/10)) + dd )%7;
     var day=(day.toFixed(0));
     // console.log(day);
+    if(day==0 && gender=='female'){
+        document.getElementById('valu').innerHTML='Akosua'  
+    }else if(day==1 && gender=='female'){
+         document.getElementById('valu').innerHTML='Adwoa'
+    }else if(day==2 && gender=='female'){
+        document.getElementById('valu').innerHTML='Abenaa'
+   }else if(day==3 && gender=='female'){
+    document.getElementById('valu').innerHTML='Akua'
+}else if(day==4 && gender=='female'){
+    document.getElementById('valu').innerHTML='Yaa'
+}
+else if(day==5 && gender=='female'){
+    document.getElementById('valu').innerHTML='Afua'
+}
+else if(day==6 && gender=='female'){
+    document.getElementById('valu').innerHTML='Ama'
+}
